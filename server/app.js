f74afb83a83b8b6d3411261a7ccc6d6eb2a1fcd1
Max Einhorn <maxeinhorn@gmail.com>
2022-05-22 16:58:03 -0400

    logical folder system implemented

--- server/app.js
+++ server/app.js
@@ -8,16 +8,16 @@ const compression = require('compression');
 
 // app.use(bodyParser.json());
 // app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/home.html'));
+  res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
