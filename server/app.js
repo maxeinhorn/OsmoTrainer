3d30ceab579cc185b2db22dddc75ead70a3d8f14
Max Einhorn <maxeinhorn@gmail.com>
2022-05-22 16:18:26 -0400

    gain again

--- server/app.js
+++ server/app.js
@@ -8,14 +8,14 @@ const compression = require('compression');
 
 // app.use(bodyParser.json());
 // app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/views/home.html'));
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
 app.listen(port, () => {
