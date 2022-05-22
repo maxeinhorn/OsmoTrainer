e5abd38dfe83de004fb6a938b2c1f04a92b9eca8
Max Einhorn <maxeinhorn@gmail.com>
2022-05-22 19:16:55 -0400

    cosmwasm calling functional

---
+++ logic2.js
@@ -0,0 +1,23 @@
+import { CosmWasmClient } from "cosmwasm";
+
+// This is your rpc endpoint
+const rpcEndpoint = "http://localhost:26657/";
+
+// async function increaseCounter() {
+//   const client = await CosmWasmClient.connect(rpcEndpoint);
+
+//   const increaseCounter = await client.queryContractSmart("osmo1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqq7fzxcr",{"increment":{}})
+//   console.log(increaseCounter);
+// }
+
+// increaseCounter();
+
+
+async function getCount() {
+  const client = await CosmWasmClient.connect(rpcEndpoint);
+
+  const getCount = await client.queryContractSmart("osmo1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqq7fzxcr",{ "get_count": {}})
+  console.log(getCount);
+}
+
+getCount();
