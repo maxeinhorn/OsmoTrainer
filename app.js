3fb86b42a09a450eef76abdd474c0ec05c944e4d
Max Einhorn <maxeinhorn@gmail.com>
2022-05-22 16:01:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const bodyParser = require('body-parser');
+// const db = require('../database/index.js');
+const compression = require('compression');
+
+// app.use(bodyParser.json());
+// app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/home.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
