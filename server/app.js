d3a72bef6e09344797b3a184cac2b84d498577f2
Max Einhorn <maxeinhorn@gmail.com>
2022-05-22 18:08:17 -0400

    Revert "webpack implementation fail"
    This reverts commit 93112a2b73bd5d7e53aaf7118d49d6431b746f84.

--- server/app.js
+++ server/app.js
@@ -8,14 +8,14 @@ const compression = require('compression');
 
 // app.use(bodyParser.json());
 // app.use(compression());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
 app.listen(port, () => {
